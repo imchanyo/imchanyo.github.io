d885bf138b46c6c05a164366092eac62e2721119
imchanyo <chanyoung3320@gmail.com>
2023-12-12 16:05:24 +0900

    Updates

---
+++ _next/static/UUXV93bXMrYYjqHKCTL3L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-cd3fdbed08fd28ce.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
