25f0fecbc39c2543ac68d3f2c96ccadbbeb5581f
imchanyo <chanyoung3320@gmail.com>
2023-12-12 16:16:01 +0900

    수정

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
             .map((post, i) => {
               return (
                 <div key={i} className="mb-4">
-                  <Link href={`/${post.href}`} className="link">
+                  <Link href={`${post.href}`} className="link">
                     {post.title}
                   </Link>
                   <p className="mt-0.5 text-sm text-gray-11">
