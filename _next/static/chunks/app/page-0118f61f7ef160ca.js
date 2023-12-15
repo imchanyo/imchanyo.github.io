0476fa487e55a94ad78c9dd150988b0f220352c7
imchanyo <chanyoung3320@gmail.com>
2023-12-15 15:53:48 +0900

    Updates

---
+++ _next/static/chunks/app/page-0118f61f7ef160ca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9742:function(n,e,t){Promise.resolve().then(t.t.bind(t,8853,23)),Promise.resolve().then(t.t.bind(t,4430,23))},4430:function(){}},function(n){n.O(0,[853,189,927,744],function(){return n(n.s=9742)}),_N_E=n.O()}]);
