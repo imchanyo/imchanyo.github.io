be3d0e602e485d07f718913ca121a3aae923a02a
imchanyo <chanyoung3320@gmail.com>
2024-01-11 16:56:56 +0900

    test

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,24 +2,18 @@
 
 import { ThemeProvider } from 'next-themes';
 import { KBarProvider } from 'kbar';
-import useKBarAction from '~/libs/useKBarAction';
+
 import dynamic from 'next/dynamic';
 
-const KBar = dynamic(() => import('~/components/kbar'), { ssr: false });
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  const actions = useKBarAction();
   return (
-    <KBarProvider actions={actions}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <KBar />
-        {children}
-      </ThemeProvider>
-    </KBarProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <KBarProvider actions={[]}>{children}</KBarProvider>
+    </ThemeProvider>
   );
 }
