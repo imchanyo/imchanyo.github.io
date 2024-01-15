d51ea32604778e136f73181cd70a9600cbd1e747
imchanyo <chanyoung3320@gmail.com>
2024-01-15 16:28:05 +0900

    Updates

---
+++ _next/static/RbhbN9tGsjG0j0MlUNO_o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7232725336e04764.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
